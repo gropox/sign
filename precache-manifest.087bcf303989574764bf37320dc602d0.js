e1c65f3db15fe264361a79bfc13693f2615ff262
Georg Sawtschuk <georg.the.moon@googlemail.com>
2018-12-15 13:25:59 +0100

    Updates

---
+++ precache-manifest.087bcf303989574764bf37320dc602d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd3b34e22b7d4cb2f4b6",
+    "url": "/sign/static/js/runtime~main.dd3b34e2.js"
+  },
+  {
+    "revision": "afa5043d9aa9103fa2fa",
+    "url": "/sign/static/js/main.afa5043d.chunk.js"
+  },
+  {
+    "revision": "0f85b731a5e6c0e059bd",
+    "url": "/sign/static/js/1.0f85b731.chunk.js"
+  },
+  {
+    "revision": "afa5043d9aa9103fa2fa",
+    "url": "/sign/static/css/main.9a668395.chunk.css"
+  },
+  {
+    "revision": "0f85b731a5e6c0e059bd",
+    "url": "/sign/static/css/1.45eeb80c.chunk.css"
+  },
+  {
+    "revision": "e4be2062c3004934b72600a9fda4158a",
+    "url": "/sign/index.html"
+  }
+];
