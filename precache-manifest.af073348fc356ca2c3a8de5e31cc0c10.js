70e719a4649bc63402a7633df21d8f49d5ac38bb
Georg Sawtschuk <georg.the.moon@googlemail.com>
2018-12-15 18:32:16 +0100

    Updates

---
+++ precache-manifest.af073348fc356ca2c3a8de5e31cc0c10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd3b34e22b7d4cb2f4b6",
+    "url": "/sign/static/js/runtime~main.dd3b34e2.js"
+  },
+  {
+    "revision": "9cf259ae6703db7607de",
+    "url": "/sign/static/js/main.9cf259ae.chunk.js"
+  },
+  {
+    "revision": "b21ea0353d8cc9d629e3",
+    "url": "/sign/static/js/1.b21ea035.chunk.js"
+  },
+  {
+    "revision": "9cf259ae6703db7607de",
+    "url": "/sign/static/css/main.6d4d8d8f.chunk.css"
+  },
+  {
+    "revision": "b21ea0353d8cc9d629e3",
+    "url": "/sign/static/css/1.cd7f7ad9.chunk.css"
+  },
+  {
+    "revision": "042eecfc987a858e221b85735ce2138e",
+    "url": "/sign/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sign/precache-manifest.5ac92495e8c74807a73061bad1abd800.js"
+  "/sign/precache-manifest.af073348fc356ca2c3a8de5e31cc0c10.js"
 );
 
 workbox.clientsClaim();
