c8b3cd48b37db8aedd0da6e21fa9421a8f3d739d
Georg Sawtschuk <georg.the.moon@googlemail.com>
2019-02-20 19:16:11 +0100

    hide toggler

--- src/components/SignForm.jsx
+++ src/components/SignForm.jsx
@@ -149,7 +149,7 @@ export class SignForm extends Component {
                         {...this.props}
                     />
                 }
-                <BottomToggler />
+                {(!this.state.sending && !this.state.result) &&  <BottomToggler /> }
             </div>
         )
     }
