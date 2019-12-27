da8a01d3f4a0188b914952c086d1f77c1b575c5c
gropox <georg.the.moon@gmail.com>
2019-12-27 10:54:55 +0100

    Updates

---
+++ precache-manifest.aaf8f7467d17db680a343530785511b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f444e696401d18601ac",
+    "url": "/sign/static/css/main.d2464402.chunk.css"
+  },
+  {
+    "revision": "6f444e696401d18601ac",
+    "url": "/sign/static/js/main.13e30469.chunk.js"
+  },
+  {
+    "revision": "507135e3b2a73c5a1743",
+    "url": "/sign/static/js/runtime~main.4f221652.js"
+  },
+  {
+    "revision": "fa81a9b64d3f8ec69ce6",
+    "url": "/sign/static/css/2.67a94abe.chunk.css"
+  },
+  {
+    "revision": "fa81a9b64d3f8ec69ce6",
+    "url": "/sign/static/js/2.5fa6d60d.chunk.js"
+  },
+  {
+    "revision": "75962ce046808ef2111b312937f26ca0",
+    "url": "/sign/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sign/precache-manifest.4b8cd766bed76a87f0e12ffe6e43f3e1.js"
+  "/sign/precache-manifest.aaf8f7467d17db680a343530785511b7.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/sign/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
